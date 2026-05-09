61e10214bd7ab29ac11e6c2fe2f305ee7d2bdf9f
bercho <you@example.comberprado@gmail.com>
2026-05-09 08:42:55 -0400

    ✨ Agregar imagen de Android 192x192 en la carpeta de imágenes estáticas.

--- static/sw.js
+++ static/sw.js
@@ -4,7 +4,7 @@
 //             Network First para llamadas a la API.
 // ============================================================
 
-const CACHE_NAME = 'backstage-v8.0';
+const CACHE_NAME = 'backstage-v8.4';
 
 // Archivos que se pre-cachean al instalar la PWA
 const ASSETS_TO_CACHE = [
