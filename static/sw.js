8f93ac465719f92c6d97c81fc0f6b22fb0f96ef4
bercho <you@example.comberprado@gmail.com>
2026-05-09 02:30:45 -0400

    experiment: gradiente magenta-cyan en boton login + reducir separacion logo-form v5.9

--- static/sw.js
+++ static/sw.js
@@ -4,7 +4,7 @@
 //             Network First para llamadas a la API.
 // ============================================================
 
-const CACHE_NAME = 'backstage-v5.8';
+const CACHE_NAME = 'backstage-v5.9';
 
 // Archivos que se pre-cachean al instalar la PWA
 const ASSETS_TO_CACHE = [
